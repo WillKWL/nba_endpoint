4cf0c2cbe20518f20266c1d108873b90272e430a
WillKWL <william05kwok@hotmail.com>
2022-11-19 16:47:27 -0500

    update retrieve

--- nba_endpoint/statics.py
+++ nba_endpoint/statics.py
@@ -1,12 +1,8 @@
 import os
 import pandas as pd 
 
-def retrieve_leaguedashplayerbiostats():
-    file_path = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(file_path)
-    os.chdir('../data')
-    
-    output = pd.read_pickle("nba_playerbiostats.pkl")
+def retrieve_leaguedashplayerbiostats():    
+    output = pd.read_pickle("https://github.com/WillKWL/nba_endpoint/blob/master/data/nba_playerbiostats.pkl?raw=true")
     seasons = output.SEASON.unique()
     print(f"Data retrieved from {min(seasons)} to {max(seasons)}")
     return output
