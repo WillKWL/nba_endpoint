f2691ee58ef9462cd03ddd06c6d0394a2d77187c
WillKWL <william05kwok@hotmail.com>
2022-11-19 15:53:16 -0500

    create package to get NBA stats

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nba_endpoint',
+    version='0.0.1',
+    description='My package to get data from NBA.stats',
+    url='https://github.com/WillKWL/nba_endpoint.git',
+    author='William Kwok',
+    author_email='william05kwok@hotmail.com',
+    license='unlicense',
+    packages=['nba_endpoint'],
+    zip_safe=False
+)
